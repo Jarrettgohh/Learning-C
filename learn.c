caea3d8be7387d23bb3494e2326ac5bc7a80c8e2
Jarrett Goh <Jarrettgoh.xz@gmail.com>
2021-05-09 16:06:05 +0800

    # First commit

---
+++ learn.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main(void)
+{
+ printf("Score A\tScore B\tScore C");
+ printf("\nThe computer\'s bell sound like this: \a\n"); // \a gives the bell sound
+ printf("\'Let\'s go for a movie tonight\'\n");
+ printf("%s bought %d oranges, each orange costs $%.2f", "Peter", 10, 0.80);
+}
