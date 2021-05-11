7fa05cfd8e9d7153d06ea9f9fca236d6fd123b9a
Jarrett Goh <Jarrettgoh.xz@gmail.com>
2021-05-11 11:20:16 +0800

    # Added simple example for arrays

--- learn.c
+++ learn.c
@@ -8,6 +8,8 @@ void main(void)
  char name[] = "Jarrett";
  int oranges = 10;
  float price = 0.80;
+ int arrayTest[10] = {1, 2, 3, 4};
 
- printf("%s bought %d oranges, each orange costs $%.2f", name, oranges, price); // %.2f is 2 d.p float
+ printf("%s bought %d oranges, each orange costs $%.2f\n", name, oranges, price); // %.2f is 2 d.p float
+ printf("%d", arrayTest[1]);
 }
